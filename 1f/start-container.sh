cc178ea61e5006a40bfa0a2046a64f5ba9ed923d
jackalcooper <jackalcooper@gmail.com>
2022-09-16 15:04:29 +0800

    add

--- 1f/start-container.sh
+++ 1f/start-container.sh
@@ -3,4 +3,5 @@ set -e
 docker run -it --rm --gpus all \
     -v $PWD/outputs/:/app/outputs/ \
     -v $HOME/.cache/huggingface:/root/.cache/huggingface \
+    -v $PWD:$PWD \
     oneflowinc/sd1f:latest bash
